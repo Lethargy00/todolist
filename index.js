f149d3fe981da7bfb4fa9c0fd345a318b2f613ac
Linus Isacsson <lisacmobil@gmail.com>
2023-10-06 15:50:09 +0200

    setup the css

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+
+//main page
 app.get("/", (req, res) => {
 
     res.render("index.ejs");
 
 });
 
+//send a message whenever i start nodemon index.js
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
